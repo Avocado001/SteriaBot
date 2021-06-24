7ff0346e28a0877c904641c12da4d92a2ff56231
Avocado001 <69569158+Avocado001@users.noreply.github.com>
2021-06-24 23:58:36 +0800

    Update animequote.js

--- commands/commands/fun/animequote.js
+++ commands/commands/fun/animequote.js
@@ -18,7 +18,7 @@ module.exports = {
             const embed = new MessageEmbed()
                 .setColor("RANDOM")
                 .addField(`Quoted from ${quote.anime}`, `**"${quote.quote}"**\n\n-*${quote.name}*`)
-                .setFooter("Bot Made By OblivionGhoul#5842", "https://i.imgur.com/Ivtf7GP.png")
+                .setFooter("Bot Made By Avocado#1747", "https://64.media.tumblr.com/674fec8a19332cdf495a517bf912bab4/8cbd38afb32ebf1a-1c/s540x810/8f112de30862a47cd0a2a164f1dadd735e25c260.gifv")
 
             if (!results) {
             } else if (!results[0].attributes) {
