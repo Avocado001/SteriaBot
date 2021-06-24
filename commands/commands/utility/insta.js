f9588b457eab18bc9baef118cf0e2c60c42df7ac
Avocado001 <69569158+Avocado001@users.noreply.github.com>
2021-06-25 00:14:50 +0800

    Update insta.js

--- commands/commands/utility/insta.js
+++ commands/commands/utility/insta.js
@@ -11,7 +11,7 @@ module.exports = {
             .setColor('#FF2D00')
             .setThumbnail('https://scontent.fmnl8-2.fna.fbcdn.net/v/t1.6435-9/187845552_167251435403889_764366581181602099_n.jpg?_nc_cat=103&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=bhoIqujwveAAX-_W-Cn&tn=8c86R4RZ0HvKHvY_&_nc_ht=scontent.fmnl8-2.fna&oh=9be4d5da946295ae95bcb31451656b96&oe=60D97EA1')
             .setImage('https://scontent.fmnl8-2.fna.fbcdn.net/v/t1.6435-9/187459167_169230948539271_2601612494169101438_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=e3f864&_nc_ohc=o575k65aYxkAX_tAMK2&tn=8c86R4RZ0HvKHvY_&_nc_ht=scontent.fmnl8-2.fna&oh=7dc0dcbb2ae4ffa2884a06a41a2aff2c&oe=60D9F942')
-            .setFooter('Please Like, Share and Subscribe', 'https://media.discordapp.net/attachments/762874855880130621/857654753068843008/ny001.gif')
+            .setFooter('Please Like, Follow and Share the Facebook Page!', 'https://media.discordapp.net/attachments/762874855880130621/857654753068843008/ny001.gif')
             .setTimestamp()
         
         try {
